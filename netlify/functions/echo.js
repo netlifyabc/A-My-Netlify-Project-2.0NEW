b04367838ff4cb8b180e2cf8bb5cae82da451b01
Ethannoma27 <Ethannoma27@hotmail.com>
2025-07-25 11:43:20 +0800

    Initial commit

--- netlify/functions/echo.js
+++ netlify/functions/echo.js
@@ -1,6 +1,6 @@
-exports.handler = async function (event) {
+exports.handler = async function(event) {
   const headers = {
-    'Access-Control-Allow-Origin': 'https://netlifyabc.github.io', // ✅ 只写域名
+    'Access-Control-Allow-Origin': '*',  // 先用星号测试是否通过CORS
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Allow-Methods': 'POST, OPTIONS',
     'Content-Type': 'application/json',
@@ -14,22 +14,12 @@ exports.handler = async function (event) {
     };
   }
 
-  if (event.httpMethod !== 'POST') {
-    return {
-      statusCode: 405,
-      headers,
-      body: JSON.stringify({ error: 'Method Not Allowed' }),
-    };
-  }
-
   return {
     statusCode: 200,
     headers,
-    body: JSON.stringify({ message: 'Echo OK', received: event.body }),
+    body: JSON.stringify({ message: 'Hello from Netlify Function!' }),
   };
 };
-
-
 
 
 
